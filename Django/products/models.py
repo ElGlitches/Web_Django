790149f31df48061871541f565194a88876b503c
El Glitches <ivanduranluengo@gmail.com>
2023-04-20 17:34:11 -0400

    productos

--- Django/products/models.py
+++ Django/products/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Products(models.Model):
+class Product(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2,default=0.0)
+    price = models.DecimalField(max_digits=10, decimal_places=0.0 ,default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
