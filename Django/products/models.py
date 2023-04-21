2cd6ad4e532440cdb9fdeb2c578e9e0db4979539
El Glitches <ivanduranluengo@gmail.com>
2023-04-20 23:29:55 -0400

    slug

--- Django/products/models.py
+++ Django/products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2 ,default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(unique=True, max_length=200, blank=False,null=False)
 
     def __str__(self):
         return self.title
--- Django/products/urls.py
+++ Django/products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('<pk>',views.ProductDetailView.as_view(), name='product'),
+    path('<slug:slug>',views.ProductDetailView.as_view(), name='product'),
 ]
