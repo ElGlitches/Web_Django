757e93c95ffe75e1cd7402c7193e017099ffa20e
El Glitches <ivanduranluengo@gmail.com>
2023-04-20 23:19:01 -0400

    Detailview

--- Django/products/views.py
+++ Django/products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from.models import Product
 
 # Create your views here.
@@ -12,5 +13,16 @@ class ProductListView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['mensaje'] = 'Productos'
+        return context
+
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
         return context
 
