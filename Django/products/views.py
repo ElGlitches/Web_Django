3ebb206eec85dd1e59cb425857496e858b845dec
El Glitches <ivanduranluengo@gmail.com>
2023-04-21 17:52:52 -0400

    Barra de usqueda

--- Django/products/views.py
+++ Django/products/views.py
@@ -25,4 +25,19 @@ class ProductDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         
         return context
+    
+
+class ProductSearchListView(ListView):
+    template_name = 'products/search.html'
+    
+    def get_queryset(self):
+        return Product.objects.filter(title__icontains=self.query())
+    
+    def query(self):
+        return self.request.GET.get('i')
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['query'] = self.query()
+        return context
 
