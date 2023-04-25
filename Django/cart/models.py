710cffdd5724012763701e2bab0620e46220c9f4
El Glitches <ivanduranluengo@gmail.com>
2023-04-24 23:51:24 -0400

    None

--- Django/cart/models.py
+++ Django/cart/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from users.models import User
 from products.models import Product
+from django.db.models.signals import pre_save
+import uuid
 
 # Create your models here.
 
 class Cart(models.Model):
+    cart_id = models.CharField(max_length=100, null=True,blank=True, unique=True)
     user = models.ForeignKey(User, null= True,blank=True,on_delete=models.CASCADE)
     product = models.ManyToManyField(Product)
     subtotal = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
@@ -12,4 +15,11 @@ class Cart(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return []
+        return self.cart_id
+    
+
+def set_cart_id(sender, instance, *args, **kwargs):
+    if not instance.cart_id:
+        instance.cart_id = str(uuid.uuid4())
+
+pre_save.connect(set_cart_id, sender=Cart)
--- Django/cart/views.py
+++ Django/cart/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Cart
 
 # Create your views here.
 
 def cart(request):
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+    cart = Cart.objects.filter(cart_id=cart_id).first()
+
+    if cart is None:
+        cart = Cart.objects.create(user=user)
+    
+    if user and cart.user is None:
+        cart.user = user
+        cart.save()
+    
+    request.session['cart_id'] = cart.id
+    
     return render(request, 'cart/cart.html',{}) 
